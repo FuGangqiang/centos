f72bc1999b56e453ccd69af194f38547593de5a2
FuGangqiang <fu_gangqiang@qq.com>
2018-07-15 11:07:48 +0800

    customize

--- custom-script.sh
+++ custom-script.sh
@@ -7,3 +7,33 @@ set -eux
 #
 # For example:
 # yum install -y curl wget git tmux firefox xvfb
+
+cat > /etc/yum.repos.d/CentOS-Base.repo <<EOF
+[base]
+name=CentOS-\$releasever - Base
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/os/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-\$releasever - Updates
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/updates/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[extras]
+name=CentOS-\$releasever - Extras
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/extras/\$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-\$releasever - Plus
+baseurl=https://mirrors.ustc.edu.cn/centos/\$releasever/centosplus/\$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+yum makecache
+yum -y update
